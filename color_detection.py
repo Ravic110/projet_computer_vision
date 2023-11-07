ef894eac7b5257d83ca3055c6f5434d8a47125cd
victorien <andriavict@gmail.com>
2023-11-07 23:30:02 +0300

    color_detector

---
+++ color_detection.py
@@ -0,0 +1,48 @@
+import cv2
+import pandas as pd
+import numpy as np
+
+IMAGE_FILE = "colorpic.jpg"
+COLORS_FILE = "colors.csv"
+
+
+class ColorIdentifier:
+    def __init__(self, image_file, colors_file):
+        self.image = self.load_image(image_file)
+        self.color_dataset = self.load_color_dataset(colors_file)
+
+    def load_image(self, image_file):
+        return cv2.imread(image_file)
+
+    def load_color_dataset(self, colors_file):
+        return pd.read_csv(colors_file, names=["color", "hex", "r", "g", "b"])
+
+    def get_color_name(self, r, g, b):
+        # Calculez la distance euclidienne entre les valeurs RVB de l'image et celles de toutes les couleurs
+        color_values = self.color_dataset[["r", "g", "b"]].values
+        color_distances = np.linalg.norm(color_values - np.array([r, g, b]), axis=1)
+        closest_color_index = np.argmin(color_distances)
+        return self.color_dataset.iloc[closest_color_index]["color"]
+
+    def handle_double_click(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            b, g, r = self.image[y, x]
+            color_name = self.get_color_name(r, g, b)
+            print(color_name)
+            cv2.putText(self.image, color_name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+    def run(self):
+        cv2.namedWindow("image")
+        cv2.setMouseCallback("image", self.handle_double_click)
+
+        while True:
+            cv2.imshow("image", self.image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):  # Attend la touche 'q' pour quitter
+                break
+
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    color_identifier = ColorIdentifier(IMAGE_FILE, COLORS_FILE)
+    color_identifier.run()
--- text_detector.py
+++ text_detector.py
@@ -3,51 +3,61 @@ import easyocr
 import tkinter as tk
 from PIL import Image, ImageTk
 
-# Initialize the text reader
-reader = easyocr.Reader(['en', 'fr'], gpu=True)
+class TextRecognitionApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Text Recognition")
 
-# Create a Tkinter window
-root = tk.Tk()
-root.title("Text Recognition")
+        self.reader = easyocr.Reader(['en', 'fr'], gpu=True)
+        self.cap = cv2.VideoCapture(0)
+        self.threshold = 0.25
 
-# Open a connection to the webcam (you can specify the camera index)
-cap = cv2.VideoCapture(0)
+        self.label = tk.Label(root)
+        self.label.pack()
 
-threshold = 0.25
+        self.running = False
 
-def update_frame():
-    ret, frame = cap.read()
-    if ret:
-        # Detect text in the frame
-        text_ = reader.readtext(frame)
+        self.create_ui()
 
-        # Filter out low-scoring detections
-        text_ = filter(lambda t: t[2] > threshold, text_)
+    def create_ui(self):
+        start_button = tk.Button(self.root, text="Start", command=self.start_capture)
+        stop_button = tk.Button(self.root, text="Stop", command=self.stop_capture)
 
-        # Draw bounding boxes around the detections
-        for t in text_:
-            bbox, text, score = t
-            bbox = [(int(x[0]), int(x[1])) for x in bbox]
-            cv2.rectangle(frame, bbox[0], bbox[1], (0, 255, 0), 5)
-            cv2.putText(frame, text, bbox[0], cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
+        start_button.pack()
+        stop_button.pack()
 
-        # Convert the OpenCV frame to a format compatible with Tkinter
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(img)
-        img = ImageTk.PhotoImage(image=img)
+    def start_capture(self):
+        self.running = True
+        self.update_frame()
 
-        # Update the label with the new frame
-        label.img = img
-        label.config(image=img)
-        label.after(10, update_frame)
+    def stop_capture(self):
+        self.running = False
 
-label = tk.Label(root)
-label.pack()
+    def update_frame(self):
+        if self.running:
+            ret, frame = self.cap.read()
+            if ret:
+                text_ = self.reader.readtext(frame)
+                text_ = filter(lambda t: t[2] > self.threshold, text_)
 
-update_frame()
+                for t in text_:
+                    bbox, text, score = t
+                    bbox = [(int(x[0]), int(x[1])) for x in bbox]
+                    cv2.rectangle(frame, bbox[0], bbox[1], (0, 255, 0), 5)
+                    cv2.putText(frame, text, bbox[0], cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
 
-root.mainloop()
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(img)
+                img = ImageTk.PhotoImage(image=img)
 
-# Release the webcam and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+                self.label.img = img
+                self.label.config(image=img)
+
+            self.label.after(10, self.update_frame)
+        else:
+            self.cap.release()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TextRecognitionApp(root)
+    root.mainloop()
