87e5e2482f97511c1d2495be638cb3485479a527
Victorien <andriavict@gmail.com>
2025-01-13 14:50:48 +0300

    restart from 0

--- color_detector/main.py
+++
@@ -1,8 +0,0 @@
-from Color_Identifier import ColorIdentifier
-
-if __name__ == "__main__":
-    try:
-        color_identifier = ColorIdentifier(r"C:\Users\utilisateur\OneDrive\Bureau\victorien\projet ND\projet_computer_vision\color_detector\data\colors.csv")   
-        color_identifier.run()
-    except Exception as e:
-        print(f"Erreur: {e}")
